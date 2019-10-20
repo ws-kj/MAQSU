41c65e8904bfdfab586043c410df63b36eb69382
Will Savage <wsavage6316@gmail.com>
2019-10-20 13:07:23 -0400

    added support for multiple legends

--- maqsu_application/maqsu_application.py
+++ maqsu_application/maqsu_application.py
@@ -1,6 +1,7 @@
 import serial
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib.ticker as plticker
 from matplotlib import style
 
 #open serial port for reciever
@@ -8,7 +9,8 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 
 #set up figure for plotting
 fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
+mqax = plt.subplot(2, 1, 1)
+dhtax = plt.subplot(2, 1, 2)
 
 #set up lines
 tplus = []
@@ -16,6 +18,8 @@ tplus = []
 mq135 = []
 mq7   = []
 mq5   = []
+temp  = []
+hum   = []
 
 #turn serial input into usable floats
 def readSerial():
@@ -36,33 +40,49 @@ def readSerial():
     return packet
 
 #func to add data to graph
-def animate(i, tplus, mq135, mq7, mq5):
+def animate(i, tplus, mq135, mq7, mq5, temp, hum):
     #get data from data_processor
     packet = readSerial()
-    print(packet)
     tplus.append(packet[0])
     mq135.append(packet[1])
     mq7.append(packet[2])
     mq5.append(packet[3])
+    temp.append(packet[4])
+    hum.append(packet[5])
 
     #plot data
-    ax.clear()
-    #ax.plot(tplus)
-    ax.plot(tplus, mq135, label="MQ-135")
-    ax.plot(tplus, mq7, label="MQ-7")
-    ax.plot(tplus, mq5, label="MQ-5")
+    mqax.clear()
+    dhtax.clear()
+
+    #plot lines
+    mqax.plot(tplus, mq135, label="MQ-135")
+    mqax.plot(tplus, mq7, label="MQ-7")
+    mqax.plot(tplus, mq5, label="MQ-5")
+
+    dhtax.plot(tplus, temp, label="Temp (F)")
+    dhtax.plot(tplus, hum, label="Humidity")
 
     #add legend
-    plt.legend(loc='upper left')
+    mqax.legend(loc='upper left')
+    dhtax.legend(loc='upper left')
 
-    ax.set_ylim(ymin=0.0, ymax=2.0)
-    plt.xticks(rotation=45, ha='right')
-    plt.title("Sensor Output")
-    plt.ylabel("MQ Sensor Relative Value")
-    plt.xlabel("Time (s)")
+    #limit displayed range to static value
+    mqax.set_ylim(ymin=0.0, ymax=2.0)
+    dhtax.set_ylim(ymin=0.0, ymax=100.0)
+
+    #set ticker range
+    dhtloc = plticker.MultipleLocator(5.0)
+    dhtax.yaxis.set_major_locator(dhtloc)
+
+    #set labels
+    mqax.set_ylabel("Pollutant Sensor Relative Value")
+    mqax.set_xlabel("Time (s)")
+
+    dhtax.set_ylabel("Weather Data")
+    dhtax.set_xlabel("Time (s)")
 
 anim = animation.FuncAnimation(fig, animate,
-        fargs=(tplus, mq135, mq7, mq5), interval=2000)
+        fargs=(tplus, mq135, mq7, mq5, temp, hum), interval=2000)
 plt.show()
 
 
